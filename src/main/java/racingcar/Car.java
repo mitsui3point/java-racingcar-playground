21aba4208bb2892b267dafda1edc3d8121c5d6e4
mitsui3point <rladbsgh0331@gmail.com>
2024-09-11 01:22:08 +0900

    feat(racingcar): add Car move

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,12 @@
 package racingcar;
 
 public class Car {
+
+    public static final int FORWARD_NUMBER = 4;
+    public static final int INITIAL_POSITION = 0;
+    private final String name;
+    private int position;
+
     public Car(final String name) {
         if (name == null) {
             throw new IllegalArgumentException("이름에 null 을 입력할 수 없습니다.");
@@ -9,5 +15,22 @@ public class Car {
         if (length > 5 || length < 1) {
             throw new IllegalArgumentException("이름은 1~5글자 사이어야 합니다.");
         }
+        this.name = name;
+        this.position = INITIAL_POSITION;
+    }
+
+    public void move() {
+        if (randomNumber() >= FORWARD_NUMBER) {
+            position++;
+        }
+    }
+
+    protected int randomNumber() {
+        return Double.valueOf(Math.random() * 10)
+                .intValue();
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,13 +1,42 @@
 package racingcar;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class CarTest {
+
+    @Test
+    void 이동() {
+        Car car = new Car("test") {
+            @Override
+            protected int randomNumber() {
+                return 4;
+            }
+        };
+        car.move();
+        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 정지() {
+        Car car = new Car("test") {
+            @Override
+            protected int randomNumber() {
+                return 0;
+            }
+        };
+        car.move();
+        Assertions.assertThat(car.getPosition()).isEqualTo(0);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"crong", "c"})
     void 자동차_이름은_1_5_글자_사이이다(String name) {
