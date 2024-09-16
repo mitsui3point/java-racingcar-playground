87bcd4f9c399b7e4843b84845d2fd5562435fade
mitsui3point <rladbsgh0331@gmail.com>
2024-09-16 17:45:54 +0900

    feat(racingcar): add car move

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,11 +1,43 @@
 package racing;
 
+import racingcar.util.NumberCreator;
 import racingcar.wrapper.Name;
+
+import java.util.Objects;
 
 public class Car {
     private final Name name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = new Name(name);
+    }
+
+    public Car(String name, int position) {
+        this.name = new Name(name);
+        this.position = position;
+    }
+
+    public void move(NumberCreator numberCreator) {
+        int forwardNumber = numberCreator.create();
+        if (forwardNumber > 9 || forwardNumber < 0) {
+            throw new IllegalArgumentException("잘못된 전진숫자입니다. 전진 숫자 0 ~ 9.");
+        }
+        if (forwardNumber >= 4) {
+            position++;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,22 +7,21 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racing.Car;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
     @Test
     void 자동차를_만든다() {
         Assertions.assertThatNoException().isThrownBy(() -> {
-            Car car = new Car("name");
+            Car car = new Car("name", 1);
         });
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1","crong"})
+    @ValueSource(strings = {"1", "crong"})
     void 자동차_이름_길이_성공(String name) {
         assertThatNoException().isThrownBy(() -> {
-            new Car(name);
+            new Car(name, 1);
         });
     }
 
@@ -31,7 +30,38 @@ public class CarTest {
     @ValueSource(strings = {"crong1"})
     void 자동차_이름_길이_실패(String name) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            new Car(name);
+            new Car(name, 1);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 자동차_전진(int forwardNumber) {
+        Car actual = new Car("car");
+        Car expectedCar = new Car("car", 1);
+
+        actual.move(() -> forwardNumber);
+
+        assertThat(actual).isEqualTo(expectedCar);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 자동차_정지(int forwardNumber) {
+        Car actual = new Car("car");
+        Car expected = new Car("car", 0);
+
+        actual.move(() -> forwardNumber);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 10, Integer.MAX_VALUE, Integer.MIN_VALUE})
+    void 자동차_예외(int forwardNumber) {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            Car actual = new Car("car");
+            actual.move(() -> forwardNumber);
         });
     }
 }
